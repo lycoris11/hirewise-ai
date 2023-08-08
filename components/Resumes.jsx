39b143c00ab41a1788b672d423274fc4e076b49b
lycoris11 <pinto9435@gmail.com>
2023-08-07 22:18:40 -0500

    Loading Animation + Back Button

--- components/Resumes.jsx
+++ components/Resumes.jsx
@@ -3,6 +3,8 @@ import '../configureAmplify';
 import UploadResume from "./UploadResume"
 import { useState, useEffect } from 'react';
 import { Storage } from 'aws-amplify';
+import { ArrowLeftIcon } from '@heroicons/react/20/solid'
+
 
 
 export default function ResumeUploads({ userIdentityId, currentJD, setMainPanelState}){
@@ -10,6 +12,7 @@ export default function ResumeUploads({ userIdentityId, currentJD, setMainPanelS
   const[resumeData, setResumeData] = useState([]);
   const[aiOutput, setAIOutput] = useState('');
   const[resumeFileForm, setResumeFileForm] = useState({resumeFileData:null});
+  const[finishedQueryingResponses, setFinishedQueryingResponses] = useState(false);
 
   const api = 'api76df32da';
 
@@ -25,8 +28,6 @@ export default function ResumeUploads({ userIdentityId, currentJD, setMainPanelS
   }, [currentJD])
 
   async function getResumes(isCancelled){
-    console.log(userIdentityId)
-    console.log(currentJD)
     const body = {body:{
       identityID:userIdentityId,
       jdName: currentJD
@@ -35,9 +36,8 @@ export default function ResumeUploads({ userIdentityId, currentJD, setMainPanelS
     try {
       const result = await API.post(api, '/db/getResume', body);
       if(!isCancelled || isCancelled === undefined){
-        const returnedData = result.response.records.map(record => record);
-        setResumeData(returnedData);
-        console.log(returnedData)
+        setResumeData(result.response.records);
+        setFinishedQueryingResponses(true);
       }
     } catch (err) {
       console.log(err);
@@ -138,17 +138,24 @@ export default function ResumeUploads({ userIdentityId, currentJD, setMainPanelS
           <p className='text-4xl leading-9 font-semibold text-white'>Welcome, Recruiter!</p>
         </div>
 
-        <button 
-          onClick={() => {openModal(true)}}
-          className='flex py-2 px-4 justify-center items-center gap-2 rounded-md bg-indigo-500 shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500'>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2.5} stroke="currentColor" className="w-5 h-5 text-white">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5" />
-          </svg>
-          <div className='text-sm leading-5 font-medium text-white whitespace-nowrap'>Upload a Resume</div>
-        </button>
+        <div className='flex gap-2'>
+          <button
+            onClick={() => {setMainPanelState("Jobs")}}
+            type="button"
+            className="rounded-full bg-indigo-600 p-2 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 transition-all duration-300 ease-in-out transform hover:-translate-x-1 focus:scale-110"
+          >
+            <ArrowLeftIcon className="h-5 w-5" aria-hidden="true" />
+          </button>
+          <button 
+            onClick={() => {openModal(true)}}
+            className='flex py-2 px-4 justify-center items-center gap-2 rounded-md bg-indigo-500 shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500'>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2.5} stroke="currentColor" className="w-5 h-5 text-white">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5" />
+            </svg>
+            <div className='text-sm leading-5 font-medium text-white whitespace-nowrap'>Upload a Resume</div>
+          </button>
+        </div>
       </div>
-
-      <button onClick={() => {setMainPanelState("Jobs")}}>BACK</button>
 
       <div className="mt-12 px-16">
         <h1 className="font-bold text-4xl text-indigo-500">Match Candidates - {currentJD}</h1>
@@ -170,13 +177,24 @@ export default function ResumeUploads({ userIdentityId, currentJD, setMainPanelS
 
           <div>
             {
-              resumeData.map((item, i)=>{
-                return (
-                  <div key={i} className='mt-4'>
-                    <p>{item[5].stringValue}</p>
+              (resumeData.length == 0 && !setFinishedQueryingResponses) ? (
+                <>
+                  <div className="flex items-center justify-center w-full h-full py-24">
+                    <div role="status">
+                      <svg aria-hidden="true" className="w-8 h-8 mr-2 text-gray-200 animate-spin fill-indigo-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/><path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/></svg>
+                      <span className="sr-only">Loading...</span>
+                    </div>
                   </div>
-                )
-              })
+                </>
+              ) : (
+                resumeData.map((item, i)=>{
+                  return (
+                    <div key={i} className='mt-4'>
+                      <p>{item[5].stringValue}</p>
+                    </div>
+                  )
+                })
+              )
             }
           </div>
         </div>
